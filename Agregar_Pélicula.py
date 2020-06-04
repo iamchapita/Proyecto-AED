9ad94f607e11bab08ebfa2b3dc770bdc9a017987
iamchapita <alejandrom646@gmail.com>
2020-06-03 18:56:01 -0600

    Delete Agregar_Pélicula.py

--- Agregar_Pélicula.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'Agregar_Pélicula.ui'
-#
-# Created by: PyQt5 UI code generator 5.14.0
-#
-# WARNING! All changes made in this file will be lost!
-
-
